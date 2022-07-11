b762aaf76f05b176333701d081c752a5a6967356
Tamim Mahmud <internationalinf24@gmail.com>
2022-07-12 00:26:33 +0600

    Slice and Splice in javaScript

---
+++ index.js
@@ -0,0 +1,12 @@
+const numbers = [2, 6, 4, 9, 7, 14, 15, 32, 15]
+console.log(numbers);
+
+//take a part in select number
+const numbers1 = [2, 6, 4, 9, 7, 14, 15, 32, 15]
+const part = numbers1.slice(2, 5);
+console.log(part);
+
+//remove parts in select number
+const numbers3 = [2, 6, 4, 9, 7, 14, 15, 32, 15]
+const removed1 = numbers3.splice(1, 2);
+console.log(removed1);
